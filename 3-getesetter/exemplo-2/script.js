1d2735df007fc73b47f5bf52f457f4e99e876321
matheusraffer <matheus.raffer@@gmail.com>
2026-05-04 10:44:59 -0300

    adicionado o `exemplo2`

---
+++ 3-getesetter/exemplo-2/script.js
@@ -0,0 +1,39 @@
+class SistemaAC  {
+    constructor() {
+        Object.seal(this); // Impede adição ou remoção de propriedades, mas permite alteração dos valores existentes
+    }
+
+    #temp = 22; // O # define um propriedade interna ES2022 (private field)
+
+    // MISSÃO 1: Criar o SETTER 'temperatura(valor)'
+    // Use if/else para validar o limite de 16 a 30.
+
+    set temperatura(valor){
+        if (valor < 16 || valor > 30){
+            window.alert("Valor invalido")
+        } else {
+            this.#temp = valor
+        }
+    }
+
+    // MISSÃO 2: Criar o GETTER 'temperatura()'
+    // Deve retornar o valor seguido da unidade "°C".
+
+    get temperatura(){
+        return this.#temp + "°C"
+    }
+};
+
+const arCondicionado = new SistemaAC();
+
+// Lógica de Integração
+function enviarComando() {
+    const input = document.getElementById('input-usuario');
+    const visor = document.getElementById('visor-temp');
+
+    // Tenta atribuir via SETTER
+    arCondicionado.temperatura = Number(input.value);
+
+    // Lê o valor via GETTER e mostra na tela
+    visor.innerText = arCondicionado.temperatura;
+}
